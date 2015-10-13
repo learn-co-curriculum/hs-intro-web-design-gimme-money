a48ee43d7065234cf00d47c2813b2e2eb9e1a062
vicfriedman <victoria.m.friedman@gmail.com>
2015-10-13 13:36:38 -0400

    solution

--- js/money.js
+++ js/money.js
@@ -1,1 +1,15 @@
 var bank_total = 200;
+
+var choice = prompt("Would you like to make a deposit or a withdraw?");
+
+if (choice == "deposit"){
+  var amount = prompt("How much money would you like to deposit?")
+  bank_total = parseInt(amount) + bank_total
+  alert ("You now have " + bank_total + " in your account.")
+} else if (choice == "withdraw"){
+  var amount = prompt("How much money would you like to withdraw?")
+  bank_total = parseInt(amount) + bank_total
+  alert ("You now have " + bank_total + " in your account.")
+}else {
+  alert("You didn't enter a valid option.")
+}
